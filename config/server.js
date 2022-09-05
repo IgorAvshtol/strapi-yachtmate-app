68592259d6459e9d58bb0a711b4eb72e01c482ba
IharAshtol <iharrwe@gmail.com>
2022-09-05 21:40:29 +0300

    fix: update gitignore;

--- config/server.js
+++ config/server.js
@@ -1,9 +1,20 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  proxy: true,
-  app: {
-    keys: env.array('APP_KEYS'),
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
     // keys: env.array('4Z1aDwt6Q0Dfm9Jbjdo9TA==','yY7/BbtoBarNsH5Z1oy7fA==','K9xhZmT74waPwYuYS2AsvA==','7c23V75daGdoQxNxsuex2A=='),
+//   },
+// });
+
+module.exports = ({ env }) => ({
+  host: '0.0.0.0',
+  port: 1337,
+  app: {
+    // keys: env.array('APP_KEYS'),
+    keys: [
+      '4Z1aDwt6Q0Dfm9Jbjdo9TA==','yY7/BbtoBarNsH5Z1oy7fA==','K9xhZmT74waPwYuYS2AsvA==','7c23V75daGdoQxNxsuex2A=='
+      ],
   },
+  url: 'http://localhost:1337'
 });
