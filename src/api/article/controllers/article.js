1941523058400874cd73c420ffd6047936f18667
IharAshtol <iharrwe@gmail.com>
2022-09-23 10:19:20 +0300

    add jsonwebtoken;

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,9 +1,87 @@
 'use strict';
 
 /**
- *  article controller
+ *  article controllers
  */
 
+const jwt = require('jsonwebtoken');
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) => ({
+  me: async (ctx) => {
+    // const token = ctx.cookies.access_token;
+    const testToken = process.env.JWT_SECRET_TEST_KEY;
+    const key = process.env.JWT_SECRET_KEY;
+    const data = jwt.verify(testToken, key);
+
+    if (!testToken) {
+      return ctx.send('Error 403');
+    } else {
+      return ctx.send(data);
+    }
+  },
+  view: async (ctx) => {
+    const { id } = ctx.params;
+
+    try {
+      const article = await strapi.entityService.findOne('api::article.article', id);
+      await strapi.entityService.update('api::article.article', id, {
+        data: {
+          view: parseInt(article.view) + 1,
+        },
+      });
+    } catch (error) {
+      return ctx.send('Error 403');
+    }
+  },
+  like: async (ctx) => {
+    // const token = ctx.cookies.access_token;
+    const testToken = process.env.JWT_SECRET_TEST_KEY;
+    const key = process.env.JWT_SECRET_KEY;
+    const data = jwt.verify(testToken, key);
+    const { id } = ctx.params;
+    const currentUserId = data.id;
+
+    if (!testToken) {
+      return ctx.send('Error 403');
+    }
+
+    try {
+      const article = await strapi.entityService.findOne('api::article.article', id);
+      const updatedArticle = await strapi.entityService.update('api::article.article', id, {
+        data: {
+          hasLiked: article.hasLiked.includes(currentUserId) ? [...article.hasLiked] : [...article.hasLiked, currentUserId],
+        },
+      });
+      return ctx.send(updatedArticle);
+    } catch {
+      return ctx.send('Error 403');
+    }
+  },
+  unlike: async (ctx) => {
+    // const token = ctx.cookies.access_token;
+    const testToken = process.env.JWT_SECRET_TEST_KEY;
+    const key = process.env.JWT_SECRET_KEY;
+    const data = jwt.verify(testToken, key);
+    const { id } = ctx.params;
+    const currentUserId = data.id;
+
+    if (!testToken) {
+      return ctx.send('Error 403');
+    }
+
+    try {
+      const article = await strapi.entityService.findOne('api::article.article', id);
+      const updatedArticle = await strapi.entityService.update('api::article.article', id, {
+        data: {
+          hasLiked: article.hasLiked.includes(currentUserId)
+            ? article.hasLiked.filter(userId => userId !== currentUserId)
+            : [...article.hasLiked],
+        },
+      });
+      return ctx.send(updatedArticle);
+    } catch (error) {
+      return ctx.send('Error 403');
+    }
+  }
+}));
--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -5,5 +5,54 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
+const defaultRouter = createCoreRouter('api::article.article');
 
-module.exports = createCoreRouter('api::article.article');
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: 'GET',
+    path: '/me',
+    handler: 'api::article.article.me',
+    config: {
+      auth: false
+    }
+  },
+  {
+    method: 'PATCH',
+    path: '/articles/:id',
+    handler: 'api::article.article.view',
+    config: {
+      auth: false
+    }
+  },
+  {
+    method: 'PATCH',
+    path: '/articles/:id/favourite',
+    handler: 'api::article.article.like',
+    config: {
+      auth: false
+    }
+  },
+  {
+    method: 'DELETE',
+    path: '/articles/:id/favourite',
+    handler: 'api::article.article.unlike',
+    config: {
+      auth: false
+    }
+  }
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
